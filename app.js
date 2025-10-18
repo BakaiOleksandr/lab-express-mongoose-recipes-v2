4e231b92692f59c20fe7eb4784efabc082720bad
Oleksandr Bakai <saniabakay@gmail.com>
2025-10-18 23:34:03 +0200

    Completed lab

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
-const express = require("express");
-const logger = require("morgan");
-
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
+const chalk = require('chalk');
+const recipeRouter = require('./routes/recipeRouter');
 
 // MIDDLEWARE
-app.use(logger("dev"));
-app.use(express.static("public"));
+app.use(logger('dev'));
+app.use(express.static('public'));
 app.use(express.json());
-
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
-
-
+const mongoDB_URL =
+  'mongodb+srv://AlexB:58998@cluster0.17vkuul.mongodb.net/RecipesData';
+mongoose
+  .connect(mongoDB_URL)
+  .then((x) => {
+    console.log(
+      chalk.blue(
+        `Connected to Mongo! Database name: "${x.connections[0].name}" \u2705`
+      )
+    );
+    console.log(chalk.blue(`Collections:`, Object.keys(x.connections[0].collections)));
+  })
+  .catch((err) => console.log(chalk.red('Error connecting to Mongo', err)));
 
 // ROUTES
+app.use('/api/recipes', recipeRouter);
+
 //  GET  / route - This is just an example route
 app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+    console.log(chalk.green('Get is working \u2705'))
+  res.send('<h1>LAB | Express Mongoose Recipes</h1>');
 });
-
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
-
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
 
-
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
-
-
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
+app.listen(3000, () => console.log(chalk.blue('My first app listening on port 3000!')));
 
 //❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
---
+++ routes/recipeRouter.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const router = express.Router();
+const Recipe = require('../models/Recipe.model');
+const chalk = require('chalk');
+
+//POST Create new recipe
+router.post('/', async (req, res) => {
+  try {
+    const existing = await Recipe.findOne({header: req.body.header});
+    if (existing) {
+      console.log(chalk.red(`${existing.header} is already exist`));
+      return res
+        .status(400)
+        .json({error: `${existing.header} is already exist`});
+    }
+    const createdRecipe = await Recipe.create({
+      header: req.body.header,
+      instructions: req.body.instructions,
+      level: req.body.level,
+      ingredients: req.body.ingredients,
+      image: req.body.image,
+      duration: req.body.duration,
+      isArchived: req.body.isArchived,
+      created: req.body.created,
+    });
+    console.log(chalk.green('New recipe was created'));
+    res.status(201).json(createdRecipe);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      console.log(chalk.red('Validation failed', err.message));
+      return res.status(400).json('Validation Error');
+    }
+    console.log(chalk.red('Can not create new recipe'));
+    return res.status(404).json({error: err.message});
+  }
+});
+//Get all recipes
+router.get('/', async (req, res) => {
+  try {
+    const getRecipes = await Recipe.find();
+    res.status(200).json(getRecipes);
+    console.log(chalk.green('Get all recipes is ok \u2705'));
+    console.log(chalk.bold('List of recipes headers:'));
+    getRecipes.forEach((el) => {
+      console.log(chalk.italic(el.header));
+    });
+  } catch (err) {
+    console.log(chalk.red('Cannot found recipes'));
+    return res.status(500).json({error: err.message});
+  }
+});
+//MODULE EXPORT
+//GET single recipe
+router.get('/:id', async (req, res) => {
+  try {
+    const recipeID = req.params.id;
+    const getRecipeId = await Recipe.findById(recipeID);
+    if (!getRecipeId) {
+      console.log(chalk.red(`Recipe was not found`));
+      return res.status(404).json({error: 'Recipe was not found'});
+    }
+    console.log(chalk.green(`${getRecipeId.header} was found`));
+    res.status(200).json(getRecipeId);
+  } catch (err) {
+    console.log(chalk.red('Can not get recipe'));
+    return res.status(500).json({error: err.message});
+  }
+});
+//UPDATE single recipe
+router.post('/:id', async (req, res) => {
+  try {
+    const recipeID = req.params.id;
+    const updatedRecipe = await Recipe.findByIdAndUpdate(recipeID, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedRecipe) {
+      console.log(chalk.red(`Recipe was not found`));
+      return res.status(404).json({error: 'Recipe was not found'});
+    }
+    console.log(chalk.green(`Recipe ${updatedRecipe.header} was updeted`));
+    res.status(200).json(updatedRecipe);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      console.log(chalk.red('Validation failed', err.message));
+      return res.status(400).json('Validation error');
+    }
+    console.log(chalk.red('Can not update recipe'));
+    return res.status(500).json({error: err.message});
+  }
+});
+//DELETE a single recipe
+router.delete('/:id', async (req, res) => {
+  try {
+    const recipeID = req.params.id;
+    const deleteRecipe = await Recipe.findByIdAndDelete(recipeID);
+    if (!deleteRecipe) {
+      console.log(chalk.red('Recipe not found'));
+      return res.status(500).json({error: 'Recipe not found'});
+    }
+    console.log(chalk.green(`Recipe ${deleteRecipe.header} was deleted`));
+    res.status(200).json(deleteRecipe);
+  } catch (err) {
+    console.log(chalk.red(`Can not delete recipe`));
+    return res.status(500).json({error: err.message});
+  }
+});
+module.exports = router;
